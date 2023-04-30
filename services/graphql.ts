6b4fe1bd37c33577a773eb25c611e588e4617018
Hp <abdullahalnoman1512.com>
2023-04-30 11:50:11 +0600

    adding

--- services/graphql.ts
+++ services/graphql.ts
@@ -1,15 +1,3 @@
-// import { GraphQLClient } from "graphql-request";
-
-// const endpoint = process.env.HASURA_PROJECT_ENDPOINT || ("https://social-next.hasura.app/v1/graphql" as string);
-
-// const client = new GraphQLClient(endpoint, {
-//   headers: {
-//     // Set any required headers here, e.g. Authorization token
-//     Authorization: `Bearer ${process.env.NEXT_PUBLIC_HASURA_TOKEN}` as string
-//   }
-// });
-
-// export default client;
 import { GraphQLClient } from "graphql-request";
 import { Session } from "next-auth";
 import { getSession } from "next-auth/react";
